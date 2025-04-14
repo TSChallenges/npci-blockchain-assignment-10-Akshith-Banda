5a3cd18527c2408c812da0aacf867b179d06cbf8
Akshith Banda <akshith.banda@gmail.com>
2025-04-15 04:58:28 +0530

    added bins and compose files

--- chaincode/drugChaincode.go
+++ chaincode/drugChaincode.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"time"
+
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
@@ -26,39 +28,170 @@ type SmartContract struct {
 	contractapi.Contract
 }
 
+func timestamp() string {
+	return time.Now().GoString()
+}
+
 // ============== MANUFACTURER FUNCTIONS ==============
-func (s *SmartContract) RegisterDrug(ctx contractapi.TransactionContextInterface, 
+func (s *SmartContract) RegisterDrug(ctx contractapi.TransactionContextInterface,
 	drugID string, name string, batchNumber string, mfgDate string, expiryDate string, composition string) error {
-	
+
 	// TODO: Verify caller is CiplaMSP
+	clientMsp, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return err
+	}
+	if clientMsp == "CDSCO" {
+		return fmt.Errorf("Drug regulator cannot register drug")
+	}
 	// TODO: Check if drug exists
+	drugBytes, err := ctx.GetStub().GetState(drugID)
+	if err != nil {
+		return err
+	}
+	if drugBytes != nil {
+		return fmt.Errorf("drug with drug id already exists")
+	}
 	// TODO: Initialize drug object with all fields
-	// TODO: Add creation event to history
+	drug := &Drug{
+		DrugID:          drugID,
+		Name:            name,
+		Manufacturer:    clientMsp,
+		BatchNumber:     batchNumber,
+		MfgDate:         mfgDate,
+		ExpiryDate:      expiryDate,
+		Composition:     composition,
+		CurrentOwner:    clientMsp,
+		Status:          "InProduction",
+		History:         []string{fmt.Sprintf("%s|%s|%s|%s|%s", timestamp(), "Manufactured", clientMsp, clientMsp, "manufactured the drug")},
+		IsRecalled:      false,
+		InspectionNotes: []string{},
+	}
+
+	drugBytes, err = json.Marshal(drug)
+	if err != nil {
+		return err
+	}
+
 	// TODO: Save to ledger
-	return fmt.Errorf("implementation pending")
+	err = ctx.GetStub().PutState(drugID, drugBytes)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Add creation event to history
+	err = ctx.GetStub().SetEvent("CreateDrug", drugBytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // ============== DISTRIBUTION FUNCTIONS ==============
 func (s *SmartContract) ShipDrug(ctx contractapi.TransactionContextInterface, drugID string, to string) error {
 	// TODO: Verify current owner is caller
+	clientMsp, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return err
+	}
+
+	drugBytes, err := ctx.GetStub().GetState(drugID)
+	if err != nil {
+		return err
+	}
+	var drug = &Drug{}
+	err = json.Unmarshal(drugBytes, drug)
+	if err != nil {
+		return err
+	}
+
+	if drug.CurrentOwner != clientMsp {
+		return fmt.Errorf("owner can only ship drugs")
+	}
 	// TODO: Update CurrentOwner and Status
+	drug.CurrentOwner = to
+	drug.Status = "shipped"
 	// TODO: Add shipment record to history
+	drug.History = append(drug.History, fmt.Sprintf("%s|%s|%s|%s|%s", timestamp(), "Shipped", clientMsp, to, "shipping the drug"))
+
+	drugBytes, err = json.Marshal(drug)
+	if err != nil {
+		return err
+	}
+
+	err = ctx.GetStub().PutState(drugID, drugBytes)
+	if err != nil {
+		return err
+	}
 	// TODO: Emit shipment event
-	return fmt.Errorf("implementation pending")
+	err = ctx.GetStub().SetEvent("shipDrug", drugBytes)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // ============== REGULATOR FUNCTIONS ==============
 func (s *SmartContract) RecallDrug(ctx contractapi.TransactionContextInterface, drugID string, reason string) error {
 	// TODO: Verify caller is CDSCOMSP
+	clientMsp, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return err
+	}
+	if clientMsp != "CDSCO" {
+		return fmt.Errorf("Drug regulator can only recall drugs")
+	}
 	// TODO: Set IsRecalled=true
+	drugBytes, err := ctx.GetStub().GetState(drugID)
+	if err != nil {
+		return err
+	}
+
+	var drug = &Drug{}
+	err = json.Unmarshal(drugBytes, drug)
+	if err != nil {
+		return err
+	}
+
+	drug.IsRecalled = true
+	drug.History = append(drug.History, fmt.Sprintf("%s|%s|%s|%s|%s", timestamp(), "Recalled", clientMsp, drug.Manufacturer, "recalling the drug"))
+	drug.InspectionNotes = append(drug.InspectionNotes, reason)
 	// TODO: Add recall note to InspectionNotes
-	return fmt.Errorf("implementation pending")
+	drugBytes, err = json.Marshal(drug)
+	if err != nil {
+		return err
+	}
+
+	err = ctx.GetStub().PutState(drugID, drugBytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // ============== COMMON FUNCTIONS ==============
 func (s *SmartContract) TrackDrug(ctx contractapi.TransactionContextInterface, drugID string) (string, error) {
 	// TODO: Return full drug history as JSON
-	return "", fmt.Errorf("implementation pending")
+	drugBytes, err := ctx.GetStub().GetState(drugID)
+	if err != nil {
+		return "", err
+	}
+
+	var drug = &Drug{}
+	err = json.Unmarshal(drugBytes, drug)
+	if err != nil {
+		return "", err
+	}
+
+	var buf *bytes.Buffer
+	err = json.Indent(buf, drugBytes, "", " ")
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
 
 func main() {
--- chaincode/go.mod
+++ chaincode/go.mod
@@ -2,17 +2,32 @@ module drugchaincode
 
 go 1.24
 
+require github.com/hyperledger/fabric-contract-api-go v1.2.2
+
 require (
-	github.com/golang/protobuf v1.3.5 // indirect
-	github.com/hyperledger/fabric v2.1.0+incompatible
-	github.com/hyperledger/fabric-chaincode-go v0.0.0-20200128192331-2d899240a7ed
-	github.com/hyperledger/fabric-protos-go v0.0.0-20200326212758-d7d9b8e1fcde
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/sykesm/zap-logfmt v0.0.3 // indirect
-	go.uber.org/zap v1.15.0 // indirect
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
-	golang.org/x/sys v0.0.0-20200327173247-9dae0f8f5775 // indirect
-	golang.org/x/text v0.3.2 // indirect
-	google.golang.org/genproto v0.0.0-20200326112834-f447254575fd // indirect
-	google.golang.org/grpc v1.28.0 // indirect
+	github.com/go-openapi/jsonpointer v0.20.0 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/spec v0.20.9 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
+	github.com/gobuffalo/envy v1.10.2 // indirect
+	github.com/gobuffalo/packd v1.0.2 // indirect
+	github.com/gobuffalo/packr v1.30.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/hyperledger/fabric-chaincode-go v0.0.0-20230731094759-d626e9ab09b9 // indirect
+	github.com/hyperledger/fabric-protos-go v0.3.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
